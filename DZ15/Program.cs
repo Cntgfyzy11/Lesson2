895984432201e5c0860dac7f707829b371414108
Emma <em0795@yandex.ru>
2022-10-21 21:29:08 +0400

    add DZ15

---
+++ DZ15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру");
+int numberA = int.Parse(Console.ReadLine());
+if(numberA == 6 && numberA == 7 || numberA > 5)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+
